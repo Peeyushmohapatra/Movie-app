75128ab7284d19e4f00d2341bceef85990c02de8
Peeyushmohapatra <peeyushm1998@gmail.com>
2023-08-31 01:48:00 +0530

    movie-app

--- src/Components/Allmovies/Allmovies.jsx
+++ src/Components/Allmovies/Allmovies.jsx
@@ -16,38 +16,38 @@ const Allmovies = () => {
           <h1>POPULAR MOVIES</h1>
 
          
-          {allMovies.popular.length !== 0 ? <Rowcards movies={allMovies.popular} /> : <Skeletons/>}
+          {allMovies.popular.length === 0 ? <Rowcards movies={allMovies.popular} /> : <div className='new_skeletons'><Skeletons/></div>}
             
         </div>
         <div className="upcoming_movie_container movies_container">
           <h1>UPCOMING MOVIES</h1>
-          {allMovies.upcoming.length !== 0 ? <Rowcards movies={allMovies.upcoming} /> : <Skeletons/>}
+          {allMovies.upcoming.length !== 0 ? <Rowcards movies={allMovies.upcoming} /> : <div className='new_skeletons'><Skeletons/></div>}
          
             
         </div>
         <div className="top_rated_movie_container movies_container">
           <h1>TOP-RATED MOVIES</h1>
-          {allMovies.topRated.length !== 0 ? <Rowcards movies={allMovies.topRated} />: <Skeletons/>}
+          {allMovies.topRated.length !== 0 ? <Rowcards movies={allMovies.topRated} />: <div className='new_skeletons'><Skeletons/></div>}
             
         </div>
         <div className="trending_movie_container movies_container">
           <h1>TRENDING MOVIES</h1>
-          {allMovies.trending.length !== 0 ? <Rowcards movies={allMovies.trending} /> : <Skeletons/>}
+          {allMovies.trending.length !== 0 ? <Rowcards movies={allMovies.trending} /> : <div className='new_skeletons'><Skeletons/></div>}
             
         </div>
         <div className="comedy_movie_container movies_container">
           <h1>COMEDY MOVIES</h1>
-          {allMovies.comedy.length !== 0 ? <Rowcards movies={allMovies.comedy} /> : <Skeletons/>}
+          {allMovies.comedy.length !== 0 ? <Rowcards movies={allMovies.comedy} /> : <div className='new_skeletons'><Skeletons/></div>}
             
         </div>
         <div className="action_movie_container movies_container">
           <h1>ACTION MOVIES</h1>
-          {allMovies.action.length !== 0 ? <Rowcards movies={allMovies.action} /> : <Skeletons/>}
+          {allMovies.action.length !== 0 ? <Rowcards movies={allMovies.action} /> : <div className='new_skeletons'><Skeletons/></div>}
             
         </div>
         <div className="action_movie_container movies_container">
           <h1>HORROR MOVIES</h1>
-          {allMovies.horror.length !== 0 ? <Rowcards movies={allMovies.horror} /> : <Skeletons/>}
+          {allMovies.horror.length !== 0 ? <Rowcards movies={allMovies.horror} /> : <div className='new_skeletons'><Skeletons/></div>}
             
         </div>
         </>
